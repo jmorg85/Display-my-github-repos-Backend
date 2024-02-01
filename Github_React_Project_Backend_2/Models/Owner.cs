0d44b294958a88dffc64aa06750d3b6c767c08a8
jmorg85 <justicemorgan147@gmail.com>
2024-02-01 14:55:51 -0600

    Added the models folder

---
+++ Github_React_Project_Backend_2/Models/Owner.cs
@@ -0,0 +1,24 @@
+﻿namespace Github_React_Project_Backend_2.Models
+{
+    public class Owner
+    {
+        public string login { get; set; }
+        public int id { get; set; }
+        public string node_id { get; set; }
+        public string avatar_url { get; set; }
+        public string gravatar_id { get; set; }
+        public string url { get; set; }
+        public string html_url { get; set; }
+        public string followers_url { get; set; }
+        public string following_url { get; set; }
+        public string gists_url { get; set; }
+        public string starred_url { get; set; }
+        public string subcriptions_url { get; set; }
+        public string organizations_url { get; set; }
+        public string repos_url { get; set; }
+        public string events_url { get; set; }
+        public string received_events_url { get; set; }
+        public string type { get; set; }
+        public bool stie_admin { get; set; }
+    }
+}
