f1de6322630f0893d6e68656701fb95c58c67084
jmorg85 <justicemorgan147@gmail.com>
2024-02-01 15:05:37 -0600

    Removed the weather forecast API and add dependency injection to the repo controller to grab info on my github repos

--- Github_React_Project_Backend_2/Program.cs
+++ Github_React_Project_Backend_2/Program.cs
@@ -1,11 +1,20 @@
+using Github_React_Project_Backend_2.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors(options => {
+    options.AddPolicy("CorsPolicy", builder =>
+        builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IGithubRepo, GithubRepo>();
 
 var app = builder.Build();
 
@@ -17,6 +26,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
